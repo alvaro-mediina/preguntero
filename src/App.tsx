19bda97efdabff64c87ffca01c9edae8c7ae6f95
alvaro-mediina <alvaro.medina@mi.unc.edu.ar>
2025-07-01 20:50:42 -0300

    feat: :sparkles: Randomizo las preguntas sin reemplazo

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,34 @@ import { useState } from "react";
 import { type Questions, parseQuestions } from "./models/Questions";
 import questionsRaw from "./data/questions.txt?raw";
 import InitialCard from "./components/InitialCard";
+import { shuffle } from "./utils/Utils";
 
 function App() {
   const [questions] = useState<Questions[]>(() => parseQuestions(questionsRaw));
-  
   // Estado para la pregunta actual, índice en el arreglo
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [remainingIndex, setRemainingIndex] = useState<number[]>(shuffle([...Array(questions.length).keys()]));
 
   // Función para elegir otra pregunta aleatoria
   const changeQuestion = () => {
-    if (questions.length <= 1) return;
-    let newIndex;
-    do {
-      newIndex = Math.floor(Math.random() * questions.length);
-    } while (newIndex === currentIndex);
-    setCurrentIndex(newIndex);
+    let nextRemaining = remainingIndex;
+
+    // Si ya se mostraron todas, remezclo
+    if (nextRemaining.length === 0) {
+      nextRemaining = shuffle([...Array(questions.length).keys()]);
+    }
+
+    // Elijo un índice aleatorio dentro de los que quedan
+    const randPos = Math.floor(Math.random() * nextRemaining.length);
+    const nextIndex = nextRemaining[randPos];
+
+    // Quito ese índice del arreglo
+    const newRemaining = [...nextRemaining];
+    newRemaining.splice(randPos, 1); // elimina 1 elemento en la posición randPos
+
+    //Seteo el índice que queda y el siguiente
+    setCurrentIndex(nextIndex);
+    setRemainingIndex(newRemaining);
   };
 
   return (
